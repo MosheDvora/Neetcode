8140b61e2205ff5a3b158923553dab5bf600409b
moshedvora <moshe.dvora@gmail.com>
2022-07-18 19:15:04 +0300

    valid sudoku 2 (great solution) change names and enumerate

--- validsudoku2.py
+++ validsudoku2.py
@@ -1,15 +1,18 @@
 def valid_sudoku(board):
     hash_board = set()
-    for i,row in enumerate(board):
-        for j in range(len(board)):
-            if row[j] != '.':
-                if  (str(row[j]) + " in row " + str(i) ) in hash_board: return False
-                if  (str(row[j]) + " in col " + str(i)) in hash_board: return False
-                if  (str(row[j]) + " in box " + str(i/3)+str(j/3)) in hash_board: return False
-                        # in hash_board: return False
-                hash_board.add(str(f"{row[j]} in row {i}"))
-                hash_board.add(str(f"{row[j]} in col {j}"))
-                hash_board.add(str(f"{row[j]} in box {i//3, j//3}"))
+    for i, row in enumerate(board):
+        for j, num in enumerate(row):
+            num = row[j]
+            if num != '.':
+                if f"{str(num)} in row {str(i)}" in hash_board:
+                    return False
+                if f"{str(num)} in col {str(j)}" in hash_board:
+                    return False
+                if f"{str(num)} in box {str(i // 3) + str(j // 3)}" in hash_board:
+                    return False
+                hash_board.add(str(f"{num} in row {i}"))
+                hash_board.add(str(f"{num} in col {j}"))
+                hash_board.add(str(f"{num} in box {i // 3, j // 3}"))
     return True
 
 
